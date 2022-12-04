356d424fa6fc61d8866a86906a3dcded12690b5c
vrizas <vrizas.magang.sekawanmedia@gmail.com>
2022-12-04 13:54:14 +0700

    update sitemap

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -8,11 +8,17 @@ export default function handler(req, res) {
       
       // generate sitemap here
       const xml = `<?xml version="1.0" encoding="UTF-8"?>
-      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> 
+      <urlset
+            xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+                  http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+      
       <url>
-        <loc>http://www.example.com/foo.html</loc>
-        <lastmod>2021-01-01</lastmod>
+        <loc>https://rebage.vercel.app/</loc>
+        <lastmod>2022-12-04T06:51:38+00:00</lastmod>
       </url>
+      
       </urlset>`
   
     res.end(xml)
